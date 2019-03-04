99eecd36ae8d2565daa97e0efc47141112784896
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-03-04 01:44:34 +0000

    Uncomment all validations in models after seeding.

--- joinusvicdata-app/app/models/funfact.rb
+++ joinusvicdata-app/app/models/funfact.rb
@@ -1,6 +1,6 @@
 class Funfact < ApplicationRecord
   belongs_to :location
 
-  # validates_presence_of :description
-  # validates_length_of :description, :minimum => 10
+  validates_presence_of :description
+  validates_length_of :description, :minimum => 10
 end
--- joinusvicdata-app/app/models/location.rb
+++ joinusvicdata-app/app/models/location.rb
@@ -3,7 +3,7 @@ class Location < ApplicationRecord
   has_many :funfacts
   has_and_belongs_to_many :keywords
 
-  # validates_presence_of :name, :address, :description
-  # validates_length_of :address, :minimum => 5
-  # validates_length_of :description, :minimum => 20
+  validates_presence_of :name, :address, :description
+  validates_length_of :address, :minimum => 5
+  validates_length_of :description, :minimum => 20
 end
