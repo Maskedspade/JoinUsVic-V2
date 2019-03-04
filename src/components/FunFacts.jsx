ab02046392a0235e55bd996b938baeb34365175d
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-03-04 22:46:38 +0000

    Make funfacts component read in funfacts.json

--- src/components/FunFacts.jsx
+++ src/components/FunFacts.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { Dropdown, } from 'semantic-ui-react';
+import {  } from 'semantic-ui-react';
 
 export default class FunFacts extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-       funfacts: null,
+       funfactList: this.props.funfactList
     };
   }
 
   // add the "Have funfacts for us?" slider
   render() {
-    return(<div>HELLO</div>);
+    return(
+      <div>HELLO</div>
+    );
   }
 }
