e12925eb9713d0fe52cfc325138d178359338ac0
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-03-04 02:10:55 +0000

    Rename App.js to App.jsx as a component to be rendered in index.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+import NavBar from '/components/NavBar';
+
+class App extends Component {
+
+  render() {
+    return (
+      <NavBar />
+    );
+  }
+}
+export default App
