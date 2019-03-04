245b6a495fdd61ca3fc4cd693aef8c788bd376a6
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-03-04 02:21:36 +0000

    Add layout description comment to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import ReactDom from 'react-dom'
 
 import NavBar from '/components/NavBar';
 
+// displays the app view
+// components: Canvas(with 3d model and product description on click), NavBar, FunFacts(with form)
+
 class App extends Component {
 
   render() {
