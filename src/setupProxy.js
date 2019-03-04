4083174be309d93a19add744b289521a90827a66
yhfreeman <yh627220@icloud.com>
2019-03-04 22:11:23 +0000

    test proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,6 @@
-const proxy = require('http-proxy-middleware')
+const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  console.log('HELLO!!!!');
   app.use(proxy('/api', { target: 'http://localhost:3000/' }))
 }
