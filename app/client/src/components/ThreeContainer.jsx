c69e40d5ceb068c81af80f5f83b0d65051657cc6
yhfreeman <yh627220@icloud.com>
2019-03-06 17:51:13 +0000

    sample code inside three container to test event listening to real DOM tree

--- app/client/src/components/ThreeContainer.jsx
+++ app/client/src/components/ThreeContainer.jsx
@@ -7,50 +7,119 @@ import THREE from './threeJSimport';
 export default class ThreeContainer extends Component {
   componentDidMount() {
     this.setupTHREE();
+
+    // because we don't have access to tag 'canvas' before realy DOM finishes its rendering,
+    // we wait till DOM's rendered and then add inline CSS by appending attribtues to 'canvas' tag
+    const canvas = document.querySelector('canvas');
+
+    canvas.style.width='100%';
+    canvas.style.height='100%';
   }
 
   setupTHREE() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // prepare renderer, camera and scene for webGL canvas
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    var renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+
+    // append 'canvas' tag inside the 'main-wrapper' div
+    const mainWrapper = document.getElementsByClassName('main-wrapper')[0];
+    mainWrapper.appendChild(renderer.domElement);
 
     window.addEventListener('resize', function() {
-      var width = window.innerWidth;
-      var height = window.innerHeight;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
       renderer.setSize(width, height);
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
     });
 
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     // create the shape
-    var geometry = new THREE.BoxGeometry(1,1,1);
+    const geometry = new THREE.BoxGeometry(1,1,1);
 
     // create a material, colour or image texture
-    var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: false});
-    var cube = new THREE.Mesh( geometry, material)
+    const material = new THREE.MeshLambertMaterial({color: 0xFFFFFF, wireframe: false});
 
-    scene.add(cube);
+    const cube1 = new THREE.Mesh( geometry, material);
 
-    camera.position.z = 3;
+    const cube2 = new THREE.Mesh( geometry, material);
+    cube2.position.x = -4;
+
+    const cube3 = new THREE.Mesh( geometry, material);
+    cube3.position.x = -8;
+
+    scene.add(cube1);
+    scene.add(cube2);
+    scene.add(cube3);
+
+    camera.position.z = 10;
+
+    // create light
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 5.0);
+    scene.add(ambientLight);
+
+    // add raycaster and mouse as 2D vector
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+
+    function onDocumentTouchStart(event) {
+      event.preventDefault();
+
+      event.clientX = event.touches[0].clientX;
+      event.clientY = event.touches[0].clientY;
+
+      onDocumentMouseDown(event);
+    }
+
+    function onDocumentMouseDown(event) {
+      event.preventDefault();
+
+      mouse.x = (event.clientX / renderer.domElement.width) * 2 - 1;
+      mouse.y = (event.clientY / renderer.domElement.height) * 2 + 1;
+
+      // update the picking ray with the camera and mouse position
+      raycaster.setFromCamera( mouse, camera );
+
+      var intersects = raycaster.intersectObjects(scene.children);
+      var color = (Math.random() * 0xFFFFFF);
+
+      for ( var i = 0; i < intersects.length; i++ ) {
+        intersects[ i ].object.material.color.set( 0xff0000 );
+      }
+    }
+
+    window.addEventListener( 'mousedown', onDocumentMouseDown, false );
+    window.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    // window.addEventListener( 'mousemove', onMouseMove, false );
 
     // game logic
-    var update = function() {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.005;
+    const update = function() {
+      cube1.rotation.x += 0.01;
+      cube1.rotation.y += 0.005;
+
+      cube2.rotation.x += 0.01;
+      cube2.rotation.y += 0.005;
+
+      cube3.rotation.x += 0.01;
+      cube3.rotation.y += 0.005;
+
     };
 
     // draw scene
-    var render = function() {
-      renderer.render(scene, camera);
+    function render() {
+
+      renderer.render( scene, camera );
+
     };
 
+    window.requestAnimationFrame(render);
+
     // run game loop (update, render, repeat)
-    var GameLoop = function() {
+    const GameLoop = function() {
       requestAnimationFrame( GameLoop );
 
       update();
