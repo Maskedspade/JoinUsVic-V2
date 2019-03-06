fa61ba2571382930046f9a7c3ac434292319a9f3
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-03-06 17:51:31 +0000

    Delete ternary operations and put the logic of waiting till props to arrive inside of Main.jsx

--- app/client/src/components/LocationDescription.jsx
+++ app/client/src/components/LocationDescription.jsx
@@ -2,26 +2,23 @@ import React, { Component } from 'react';
 import { Item, Icon } from 'semantic-ui-react';
 
 const DescriptionCard = ( {location} ) => {
-  return location ?
-    (
-      <Item className="ld-wrapper">
-        <Item.Header as='h2'>{location.name}</Item.Header>
-        <Item.Header as='a' href={location.website}>Website
-          <Icon name='angle double right' />
-        </Item.Header>
-        <Item.Meta className="p-fade-italic
-        ">{location.address}</Item.Meta>
-        <Item.Extra className="p-highlight">Rating: {location.rating}</Item.Extra>
-        <Item.Description className="p-desktop">{location.description}</Item.Description>
-        <Item.Header as='h4' className="p-highlight centered">Rate me: @add stars!@</Item.Header>
-        <Item.Header as='a'>
-          <Icon name="caret right" />
-          HideThis
-        </Item.Header>
-      </Item>
-    ) : (
-      <Item style={{ display: 'none' }}></Item>
-    )
+  return (
+    <Item className="ld-wrapper">
+      <Item.Header as='h2'>{location.name}</Item.Header>
+      <Item.Header as='a' href={location.website}>Website
+        <Icon name='angle double right' />
+      </Item.Header>
+      <Item.Meta className="p-fade-italic
+      ">{location.address}</Item.Meta>
+      <Item.Extra className="p-highlight">Rating: {location.rating}</Item.Extra>
+      <Item.Description className="p-desktop">{location.description}</Item.Description>
+      <Item.Header as='h4' className="p-highlight centered">Rate me: @add stars!@</Item.Header>
+      <Item.Header as='a'>
+        <Icon name="caret right" />
+        HideThis
+      </Item.Header>
+    </Item>
+  )
 }
 
 export default class LocationDescription extends Component {
