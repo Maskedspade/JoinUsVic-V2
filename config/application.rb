e8b5ed7c9bb631b5824b4fdf15c6ed7b417b8b8e
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-03-06 22:56:55 +0000

    Add configuration specification so that the -Flash error doesn't get thrown while calling Axios post request from React component to server

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module JoinusvicdataApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # To prevent error: NoMethodError (undefined method `flash=' for #<ActionDispatch::<>)
+    config.middleware.use ActionDispatch::Flash
   end
 end
