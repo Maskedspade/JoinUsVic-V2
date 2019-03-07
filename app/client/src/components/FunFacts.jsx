cffe2dd3e3e1964bbcebe7f3dff108b54fdf7f47
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-03-07 02:10:07 +0000

    Start the edit on funfacts page. Now sits on top of all the rest of index page

--- app/client/src/components/FunFacts.jsx
+++ app/client/src/components/FunFacts.jsx
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
-import {  } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import { Container, Divider, Header, Icon } from 'semantic-ui-react'
+
+const FunFactsBlocks = ( {funfacts} ) => {
+  return funfacts.map((funfact, i) => {
+    return (
+      <Container textAlign='justified' className="ff-container">
+        <b className="p-highlight">{funfact.user_name} says: </b>
+        <p className="p-fade-italic">{funfact.created_at}</p>
+        <Divider />
+        <p className="p-desktop">{funfact.description}</p>
+      </Container>
+    );
+  })
+}
 
 export default class FunFacts extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.convertName = this.convertName.bind(this)
+  }
+
+  convertName = ( name ) => {
+    return name === '@someone@' ? 'Someone' : name
   }
 
   // add the "Have funfacts for us?" slider
   render() {
-           // funfactList: this.props.funfactList
     return(
-      <div>
-
+      <div className="ff-wrapper">
+        <Container>
+          <a>
+            <Icon name="caret right" />
+            Have any fun facts for us?
+          </a>
+        </Container>
+        <FunFactsBlocks funfacts={this.props.funfactsList}/>
       </div>
     );
   }
