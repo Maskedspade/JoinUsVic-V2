79141b47caca305823792130f324644049120880
yhfreeman <yh627220@icloud.com>
2019-03-12 17:06:48 +0000

    submission and error handling for funfacts form and leave note for what to do next

--- app/client/src/components/FunFactForm.jsx
+++ app/client/src/components/FunFactForm.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Form, Button, Icon, TextArea } from 'semantic-ui-react'
+import axios from 'axios'
 
-const FormCard = ( {handleNevermind} ) => {
+const FormCard = ( {handleNevermind, clickToSubmit} ) => {
   return (
     <Form>
       <Form.Field>
@@ -18,7 +19,7 @@ const FormCard = ( {handleNevermind} ) => {
           <Icon name='cancel' />
           Nevermind
         </Button>
-        <Button type='submit'>
+        <Button type='submit' onClick={clickToSubmit} >
           <Icon name='paper plane' />
           Send
         </Button>
@@ -27,12 +28,51 @@ const FormCard = ( {handleNevermind} ) => {
   )
 }
 
+const checkEmpty = (content) => {
+  if (!content) {
+    return false
+  }
+  if (content.split(' ').join('') === '') {
+    return false
+  }
+  return true
+}
+
 export default class FunFactForm extends Component {
+  clickToSubmit() {
+    let nameInput = document.querySelector("form input").value
+    const factInput = document.querySelector("form TextArea").value
+
+    if (!checkEmpty(factInput)) {
+      const errorMsg = document.createElement("div")
+      errorMsg.classList.add("error-message")
+      const textNode = document.createTextNode("Funfact/Message cannot be blank, my friend!")
+      errorMsg.appendChild(textNode)
+
+      document.querySelector("form").appendChild(errorMsg)
+      return;
+    }
+
+    let nameForSubmit
+
+    if (!checkEmpty(nameInput)) {
+      nameForSubmit = "@someone@"
+    }
+
+    axios.post('api/funfacts', { funfact: {user_name: nameInput, description: factInput}})
+    .then(res => {
+      // if POST success to rails db - here the child triggers a parents setState so new funfact can be rendered as well
+      alert(res.data.msg)
+    })
+    .catch(error => console.log(error))
+  }
+
+
   render() {
     const { handleNevermind } = this.props
 
     return(
-      <FormCard handleNevermind={handleNevermind}/>
+      <FormCard handleNevermind={handleNevermind} clickToSubmit={this.clickToSubmit}/>
     )
   }
 }
